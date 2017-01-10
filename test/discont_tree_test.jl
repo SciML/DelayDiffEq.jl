1988becc68f8f33b11342283b6dc13a3b96a95cb
ChrisRackauckas <me@chrisrackauckas.com>
2017-01-10 11:09:36 -0800

    better discontinuity tree

--- test/discont_tree_test.jl
+++ test/discont_tree_test.jl
@@ -1,9 +1,9 @@
-using DelayDiffEq
+using DelayDiffEq, OrdinaryDiffEq, Base.Test
 lags = [1//5,1//2]
 alg = BS3()
 start_val = 1
 
-disc_tree = DelayDiffEq.compute_discontinuity_tree(lags,alg,start_val)
+disc_tree = sort(DelayDiffEq.compute_discontinuity_tree(lags,alg,start_val))
 
-true_val = 1 .+ [1//2,1//5,1//1,2//5,7//10,3//2,3//5,6//5,9//10]
+true_val = sort(1 .+ [1//2,1//5,1//1,2//5,7//10,3//2,3//5,6//5,9//10])
 @test disc_tree == true_val
