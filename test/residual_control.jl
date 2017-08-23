0b493c33f12142e4e18a7f63517c443b289cc764
ChrisRackauckas <accounts@chrisrackauckas.com>
2017-08-22 19:46:15 -0700

    fix test compatibility

--- test/residual_control.jl
+++ test/residual_control.jl
@@ -11,7 +11,8 @@ sol = solve(prob, alg)
 @test sol.errors[:l2] < 1e-4
 
 prob2 = deepcopy(prob)
-prob2.constant_lags=Rational{Int}[]
+typeof(prob2) <: DDEConstantLagProblem ? prob2.lags=Rational{Int}[] :
+                 prob2.constant_lags=Rational{Int}[]
 sol2 = solve(prob2, alg)
 
 @test sol2.errors[:l∞] < 1e-1
