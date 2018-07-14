9556418d917e0213687a280af19973338c1de654
YingboMa <mayingbo5@gmail.com>
2018-07-14 13:08:16 -0700

    Bump DelayDiffEq to Julia 0.7

--- src/DelayDiffEq.jl
+++ src/DelayDiffEq.jl
@@ -16,7 +16,8 @@ import OrdinaryDiffEq: initialize!, perform_step!, loopfooter!, loopheader!, alg
 
 import DiffEqBase: solve, solve!, init, resize!, u_cache, user_cache, du_cache, full_cache,
                    deleteat!, terminate!, u_modified!, get_proposed_dt, set_proposed_dt!,
-                   has_reinit, reinit!, auto_dt_reset!
+                   has_reinit, reinit!, auto_dt_reset!, AbstractContinuousCallback,
+                   AbstractDDEAlgorithm, AbstractDDEIntegrator, DEIntegrator, AbstractDDEProblem
 
 using OrdinaryDiffEq: Rosenbrock23Cache, Rosenbrock32Cache, ImplicitEulerCache,
     TrapezoidCache
