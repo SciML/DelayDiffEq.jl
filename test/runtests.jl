ec5ca6bf5d0ea93f1369f1d16db0f2e0f4a6faf1
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-08-18 04:32:06 -0700

    split tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,25 +8,40 @@ import DiffEqProblemLibrary.DDEProblemLibrary: prob_dde_1delay, prob_dde_1delay_
        prob_dde_2delays_long_notinplace, prob_dde_2delays_long_scalar_notinplace,
        prob_dde_mackey
 
-tests = ["discontinuities.jl",
-         "history_function.jl",
-         "constrained.jl",
-         "unconstrained.jl",
-         "parameters.jl",
-         "retcode.jl",
-         "composite_solution.jl",
-         "dependent_delays.jl",
-         "reinit.jl",
-         "saveat.jl",
-         "save_idxs.jl",
-         "events.jl",
-         "units.jl",
-         "unique_times.jl",
-         "residual_control.jl",
-         "lazy_interpolants.jl",
-         "sdirk_integrators.jl",
-         "rosenbrock_integrators.jl"]
+if haskey(ENV,"GROUP")
+   group = ENV["GROUP"]
+else
+   group = "All"
+end
 
-for test in tests
-    @time include(test)
+is_APPVEYOR = ( Sys.iswindows() && haskey(ENV,"APPVEYOR") )
+
+if group == "All" || group == "Interface"
+  tests = ["discontinuities.jl",
+           "history_function.jl",
+           "parameters.jl",
+           "retcode.jl",
+           "composite_solution.jl",
+           "dependent_delays.jl",
+           "reinit.jl",
+           "saveat.jl",
+           "save_idxs.jl",
+           "events.jl",
+           "units.jl",
+           "unique_times.jl"
+           ]
+   for test in tests
+       @time include(test)
+   end
+elseif group == "All" || group == "Integrators"
+  tests =   ["constrained.jl",
+             "unconstrained.jl",
+             "residual_control.jl",
+             "lazy_interpolants.jl",
+             "sdirk_integrators.jl",
+             "rosenbrock_integrators.jl"
+             ]
+   for test in tests
+       @time include(test)
+   end
 end
