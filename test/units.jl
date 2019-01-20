d0802ffbca5199fc3a7d9c90e8965cd00dc639de
David Widmann <dev+github@devmotion.de>
2019-01-20 03:38:39 +0100

    Test default tolerances

--- test/units.jl
+++ test/units.jl
@@ -8,35 +8,41 @@ using Unitful
 
     @testset for prob in (prob_notinplace, prob_inplace)
         @testset "correct" begin
-            # with correct units
-            alg1 = MethodOfSteps(Tsit5(), constrained=false, max_fixedpoint_iters=100,
-                                 fixedpoint_abstol=1e-7u"mN", fixedpoint_reltol=1e-4u"μN")
+            # default tolerances
+            alg1 = MethodOfSteps(Tsit5(), constrained=false, max_fixedpoint_iters=100)
             sol1 = solve(prob, alg1)
+
+            # with correct units
+            alg2 = MethodOfSteps(Tsit5(), constrained=false, max_fixedpoint_iters=100,
+                                 fixedpoint_abstol=1e-6u"N", fixedpoint_reltol=1e-3u"N")
+            sol2 = solve(prob, alg2)
+
+            @test sol1.t == sol2.t && sol1.u == sol2.u
 
             # with correct units as vectors
             if typeof(prob.u0) <: AbstractArray
-                alg2 = MethodOfSteps(Tsit5(), constrained=false, max_fixedpoint_iters=100,
-                                     fixedpoint_abstol=[1e-7u"mN"], fixedpoint_reltol=[1e-4u"μN"])
-                sol2 = solve(prob, alg2)
+                alg3 = MethodOfSteps(Tsit5(), constrained=false, max_fixedpoint_iters=100,
+                                     fixedpoint_abstol=[1e-6u"N"], fixedpoint_reltol=[1e-3u"N"])
+                sol3 = solve(prob, alg3)
 
-                @test sol1.t == sol2.t && sol1.u == sol2.u
+                @test sol1.t == sol3.t && sol1.u == sol3.u
             end
         end
 
         @testset "incorrect" begin
             # without units
             alg1 = MethodOfSteps(Tsit5(), constrained=false, max_fixedpoint_iters=100,
-                                 fixedpoint_abstol=1e-10, fixedpoint_reltol=1e-4)
+                                 fixedpoint_abstol=1e-6, fixedpoint_reltol=1e-3)
             @test_throws Unitful.DimensionError solve(prob, alg1)
 
             # with incorrect units for absolute tolerance
             alg2 = MethodOfSteps(Tsit5(), constrained=false, max_fixedpoint_iters=100,
-                                fixedpoint_abstol=1e-10u"s", fixedpoint_reltol=1e-4u"μN")
+                                fixedpoint_abstol=1e-6u"s", fixedpoint_reltol=1e-3u"N")
             @test_throws Unitful.DimensionError solve(prob, alg2)
 
             # with incorrect units for relative tolerance
             alg3 = MethodOfSteps(Tsit5(), constrained=false, max_fixedpoint_iters=100,
-                                fixedpoint_abstol=1e-7u"mN", fixedpoint_reltol=1e-4u"s")
+                                fixedpoint_abstol=1e-6u"N", fixedpoint_reltol=1e-3u"s")
             @test_throws Unitful.DimensionError solve(prob, alg3)
         end
     end
