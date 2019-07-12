754e296aab2981a51ca4785d244e4be40f8b7c44
David Widmann <dev+github@devmotion.de>
2019-07-12 17:49:08 +0200

    Add algorithm traits

--- src/alg_utils.jl
+++ src/alg_utils.jl
@@ -1,4 +1,18 @@
-alg_maximum_order(alg::AbstractMethodOfStepsAlgorithm) = alg_maximum_order(alg.alg)
-
 isconstrained(alg::AbstractMethodOfStepsAlgorithm{constrained}) where constrained =
     constrained
+
+OrdinaryDiffEq.alg_maximum_order(alg::AbstractMethodOfStepsAlgorithm) =
+  OrdinaryDiffEq.alg_maximum_order(alg.alg)
+OrdinaryDiffEq.alg_extrapolates(alg::AbstractMethodOfStepsAlgorithm) =
+  OrdinaryDiffEq.alg_extrapolates(alg.alg)
+OrdinaryDiffEq.uses_uprev(alg::AbstractMethodOfStepsAlgorithm, adaptive) = true
+
+"""
+    iscomposite(alg)
+
+Return if algorithm `alg` is a composite algorithm.
+"""
+iscomposite(alg) = false
+iscomposite(::OrdinaryDiffEq.OrdinaryDiffEqCompositeAlgorithm) = true
+iscomposite(alg::AbstractMethodOfStepsAlgorithm) = iscomposite(alg.alg)
+
