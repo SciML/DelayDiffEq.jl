3c06ddc86d5528c66fe060f17c6f915180a5c598
David Widmann <dev+github@devmotion.de>
2019-07-12 18:12:54 +0200

    Remove unneeded modifications of ODE integrator

--- src/integrators/interface.jl
+++ src/integrators/interface.jl
@@ -430,14 +430,12 @@ end
 
 function DiffEqBase.add_tstop!(integrator::DDEIntegrator,t)
   integrator.tdir * (t - integrator.t) < 0 && error("Tried to add a tstop that is behind the current time. This is strictly forbidden")
-  push!(integrator.integrator.opts.tstops,t)
-  push!(integrator.opts.tstops,t)
+  push!(integrator.opts.tstops, t)
 end
 
 function DiffEqBase.add_saveat!(integrator::DDEIntegrator,t)
   integrator.tdir * (t - integrator.t) < 0 && error("Tried to add a saveat that is behind the current time. This is strictly forbidden")
-  push!(integrator.integrator.opts.saveat,t)
-  push!(integrator.opts.saveat,t)
+  push!(integrator.opts.saveat, t)
 end
 
 @inline function DiffEqBase.get_du(integrator::DDEIntegrator)
