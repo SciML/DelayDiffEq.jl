9f1d026ad9698524cbaefe283dc811bfcf0160ee
David Widmann <dev+github@devmotion.de>
2019-08-16 00:25:58 +0200

    Add Wfact and Wfact_t support

--- src/functionwrapper.jl
+++ src/functionwrapper.jl
@@ -1,3 +1,29 @@
+# convenience macro
+macro wrap_h(signature)
+  Meta.isexpr(signature, :call) ||
+    throw(ArgumentError("signature has to be a function call expression"))
+
+  name = signature.args[1]
+  args = signature.args[2:end]
+  args_wo_h = [arg for arg in args if arg !== :h]
+  
+  quote
+    if f.$name === nothing
+      nothing
+    else
+      if isinplace(f)
+        let _f = f.$name, h = h
+          ($(args_wo_h...),) -> _f($(args...))
+        end
+      else
+        let _f = f.$name, h = h
+          ($(args_wo_h[2:end]...),) -> _f($(args[2:end]...))
+        end
+      end
+    end
+  end |> esc
+end
+
 struct ODEFunctionWrapper{iip,F,H,TMM,Ta,Tt,TJ,JP,TW,TWt,TPJ,S,TCV} <: DiffEqBase.AbstractODEFunction{iip}
   f::F
   h::H
@@ -14,26 +40,17 @@ struct ODEFunctionWrapper{iip,F,H,TMM,Ta,Tt,TJ,JP,TW,TWt,TPJ,S,TCV} <: DiffEqBas
 end
 
 function ODEFunctionWrapper(f::DDEFunction, h)
-  if f.jac === nothing
-    jac = nothing
-  else
-    if isinplace(f)
-      jac = let f_jac = f.jac, h = h
-        (J, u, p, t) -> f_jac(J, u, h, p, t)
-      end
-    else
-      jac = let f_jac = f.jac, h = h
-        (u, p, t) -> f_jac(u, h, p, t)
-      end
-    end
-  end
+  # wrap functions
+  jac = @wrap_h jac(J, u, h, p, t)
+  Wfact = @wrap_h Wfact(W, u, h, p, dtgamma, t)
+  Wfact_t = @wrap_h Wfact_t(W, u, h, p, dtgamma, t)
 
   ODEFunctionWrapper{isinplace(f),typeof(f.f),typeof(h),typeof(f.mass_matrix),
                      typeof(f.analytic),typeof(f.tgrad),typeof(jac),
-                     typeof(f.jac_prototype),typeof(f.Wfact),typeof(f.Wfact_t),
+                     typeof(f.jac_prototype),typeof(Wfact),typeof(Wfact_t),
                      typeof(f.paramjac),typeof(f.syms),typeof(f.colorvec)}(
                        f.f, h, f.mass_matrix, f.analytic, f.tgrad, jac,
-                       f.jac_prototype, f.Wfact, f.Wfact_t, f.paramjac, f.syms,
+                       f.jac_prototype, Wfact, Wfact_t, f.paramjac, f.syms,
                        f.colorvec)
 end
 
