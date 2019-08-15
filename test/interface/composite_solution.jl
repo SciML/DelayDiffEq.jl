b65dc91a79b6da09bfc591a8786a7744b2cb1420
David Widmann <dev+github@devmotion.de>
2019-08-15 22:19:50 +0200

    Add test for issue #148

--- test/interface/composite_solution.jl
+++ test/interface/composite_solution.jl
@@ -19,8 +19,30 @@ end
 
   # compare integration grid
   sol2 = solve(prob, MethodOfSteps(Tsit5()))
-  @test sol1.t == sol2.t && sol1.u == sol2.u
+  @test sol1.t == sol2.t
+  @test sol1.u == sol2.u
 
   # compare interpolation
   @test sol1(0:0.1:1) == sol2(0:0.1:1)
+end
+
+@testset "issue #148" begin
+  alg = MethodOfSteps(Tsit5())
+  compositealg = MethodOfSteps(CompositeAlgorithm((Tsit5(), RK4()), integrator -> 1))
+
+  prob = DDEProblem((du, u, h, p, t) -> -h(p, t - 1)[1], [1.0], (p, t) -> [1.0],
+                    (0.0, 5.0))
+  sol = solve(prob, alg)
+  compositesol = solve(prob, compositealg)
+  @test compositesol isa T
+  @test sol.t == compositesol.t
+  @test sol.u == compositesol.u
+
+  prob_oop = DDEProblem((u, h, p, t) -> -h(p, t - 1), [1.0], (p, t) -> [1.0],
+                        (0.0, 5.0))
+  sol_oop = solve(prob_oop, alg)
+  compositesol_oop = solve(prob_oop, compositealg)
+  @test compositesol_oop isa T
+  @test sol_oop.t == compositesol_oop.t
+  @test sol_oop.u == compositesol_oop.u
 end
