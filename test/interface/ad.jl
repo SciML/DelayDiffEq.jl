bf24fd44d9109fe0c57a5c3a8fb603baee9be675
David Widmann <dev+github@devmotion.de>
2020-05-22 20:51:30 +0200

    Test AD without delay length estimation

--- test/interface/ad.jl
+++ test/interface/ad.jl
@@ -1,9 +1,13 @@
-using DelayDiffEq, Calculus, ForwardDiff
+using DelayDiffEq
+
+import FiniteDiff
+import ForwardDiff
+
 using LinearAlgebra, Test
 
 # Hutchinson's equation
 function f(du, u, h, p, t)
-  du[1] = p[1] * u[1] * (1 - h(p, t - p[2])[1] / p[3])
+  du[1] = p[2] * u[1] * (1 - h(p, t - p[1])[1] / p[3])
   nothing
 end
 
@@ -18,11 +22,19 @@ h(p, t) = [p[4]]
     dot(diff, diff)
   end
 
+  # with delay length estimation
   p = [1.0, 1.0, 1.0, 0.0, 1.0]
-  findiff = Calculus.finite_difference(test, p)
+  findiff = FiniteDiff.finite_difference_gradient(test, p)
   fordiff = @test_logs (:warn, r"^dt <= dtmin") ForwardDiff.gradient(test, p)
-
   @test_broken findiff ≈ fordiff
+
+  # without delay length estimation
+  p = [1.0, 1.0, 0.0, 1.0]
+  findiff2 = FiniteDiff.finite_difference_gradient(p -> test(vcat(1, p)), p)
+  fordiff2 = @test_logs (:warn, r"^dt <= dtmin") ForwardDiff.gradient(p -> test(vcat(1, p)), p)
+  @test_broken findiff2 ≈ fordiff2
+
+  @test findiff[2:end] ≈ findiff2
 end
 
 @testset "Jacobian" begin
@@ -32,11 +44,19 @@ end
     solve(prob, MethodOfSteps(Tsit5()); abstol = 1e-14, reltol = 1e-14).u[end]
   end
 
+  # with delay length estimation
   p = [1.0, 1.0, 1.0, 0.0, 1.0]
-  findiff = Calculus.finite_difference_jacobian(test, p)
+  findiff = FiniteDiff.finite_difference_jacobian(test, p)
   fordiff = @test_logs (:warn, r"^dt <= dtmin") ForwardDiff.jacobian(test, p)
-
   @test_broken findiff ≈ fordiff
+
+  # without delay length estimation
+  p = [1.0, 1.0, 0.0, 1.0]
+  findiff2 = FiniteDiff.finite_difference_jacobian(p -> test(vcat(1, p)), p)
+  fordiff2 = @test_logs (:warn, r"^dt <= dtmin") ForwardDiff.jacobian(p -> test(vcat(1, p)), p)
+  @test_broken findiff2 ≈ fordiff2
+
+  @test findiff[:, 2:end] ≈ findiff2
 end
 
 @testset "Hessian" begin
@@ -48,9 +68,17 @@ end
     dot(diff, diff)
   end
 
+  # with delay length estimation
   p = [1.0, 1.0, 1.0, 0.0, 1.0]
-  findiff = Calculus.finite_difference_hessian(test, p)
+  findiff = FiniteDiff.finite_difference_hessian(test, p)
   fordiff = @test_logs (:warn, r"^dt <= dtmin") ForwardDiff.hessian(test, p)
-
   @test_broken findiff ≈ fordiff
+
+  # without delay length estimation
+  p = [1.0, 1.0, 0.0, 1.0]
+  findiff2 = FiniteDiff.finite_difference_hessian(p -> test(vcat(1, p)), p)
+  fordiff2 = @test_logs (:warn, r"^dt <= dtmin") ForwardDiff.hessian(p -> test(vcat(1, p)), p)
+  @test_broken findiff2 ≈ fordiff2
+
+  @test findiff[2:end, 2:end] ≈ findiff2
 end
