07de57fedfdd07077e4d709783703baaa7161e9f
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-05-24 12:14:20 -0600

    Extend the SciMLBase traits

--- src/alg_utils.jl
+++ src/alg_utils.jl
@@ -1,3 +1,13 @@
+## SciMLBase Trait Definitions
+
+SciMLBase.isautodifferentiable(alg::AbstractMethodOfStepsAlgorithm) = SciMLBase.isautodifferentiable(alg.alg)
+SciMLBase.allows_arbitrary_number_types(alg::AbstractMethodOfStepsAlgorithm) = SciMLBase.allows_arbitrary_number_types(alg.alg)
+SciMLBase.allowscomplex(alg::AbstractMethodOfStepsAlgorithm) = SciMLBase.allowscomplex(alg.alg)
+SciMLBase.isdiscrete(alg::AbstractMethodOfStepsAlgorithm) = SciMLBase.isdiscrete(alg.alg)
+SciMLBase.isadaptive(alg::AbstractMethodOfStepsAlgorithm) = SciMLBase.isadaptive(alg.alg)
+
+## DelayDiffEq Internal Traits
+
 isconstrained(alg::AbstractMethodOfStepsAlgorithm{constrained}) where constrained =
     constrained
 OrdinaryDiffEq.uses_uprev(alg::AbstractMethodOfStepsAlgorithm, adaptive) = true
@@ -5,7 +15,6 @@ OrdinaryDiffEq.uses_uprev(alg::AbstractMethodOfStepsAlgorithm, adaptive) = true
 OrdinaryDiffEq.isimplicit(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.isimplicit(alg.alg)
 OrdinaryDiffEq.isdtchangeable(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.isdtchangeable(alg.alg)
 OrdinaryDiffEq.ismultistep(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.ismultistep(alg.alg)
-OrdinaryDiffEq.isadaptive(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.isadaptive(alg.alg)
 OrdinaryDiffEq.isautoswitch(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.isautoswitch(alg.alg)
 OrdinaryDiffEq.get_chunksize(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.get_chunksize(alg.alg)
 OrdinaryDiffEq.get_chunksize_int(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.get_chunksize_int(alg.alg)
@@ -17,8 +26,6 @@ OrdinaryDiffEq.alg_extrapolates(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryD
 OrdinaryDiffEq.alg_order(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.alg_order(alg.alg)
 OrdinaryDiffEq.alg_maximum_order(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.alg_maximum_order(alg.alg)
 OrdinaryDiffEq.alg_adaptive_order(alg::AbstractMethodOfStepsAlgorithm) = OrdinaryDiffEq.alg_adaptive_order(alg.alg)
-
-SciMLBase.isautodifferentiable(alg::AbstractMethodOfStepsAlgorithm) = true
 
 """
     iscomposite(alg)
