d884c07d3444f35832d77cbdca35e889b2baf85b
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-12-24 11:37:04 -0500

    Remove unstable method from tests

--- test/integrators/cache.jl
+++ test/integrators/cache.jl
@@ -43,9 +43,6 @@ println("Check some other integrators")
 println("Rosenbrock23")
 @test_nowarn solve(prob, MethodOfSteps(Rosenbrock23(chunk_size = 1)); dt = 0.5)
 
-println("Rosenbrock32")
-@test_nowarn solve(prob, MethodOfSteps(Rosenbrock32(chunk_size = 1)); dt = 0.5)
-
 for alg in CACHE_TEST_ALGS
     println(nameof(typeof(alg)))
     @test_nowarn solve(prob, MethodOfSteps(alg); dt = 0.5)
