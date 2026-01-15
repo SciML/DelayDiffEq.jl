4b155428add9d2fb2563b2c677aa0f5dc70f2538
jClugstor <jadonclugston@gmail.com>
2026-01-15 12:30:45 -0500

    use verbosity macro

--- src/verbosity.jl
+++ src/verbosity.jl
@@ -57,97 +57,28 @@ verbose = DDEVerbosity(
 )
 ```
 """
-@concrete struct DDEVerbosity <: AbstractVerbositySpecifier
-    # ODE solver verbosity
-    ode_verbosity
-    # Delay-specific options
-    discontinuity_tracking
-    delay_evaluation
-    constrained_step
-    residual_control
-    neutral_delay
-    state_dependent_delay
-end
-
-# Group classifications
-const delay_specific_options = (:discontinuity_tracking, :delay_evaluation,
-    :constrained_step, :residual_control, :neutral_delay, :state_dependent_delay)
-
-function option_group(option::Symbol)
-    if option in delay_specific_options
-        return :delay_specific
-    else
-        error("Unknown verbosity option: $option")
-    end
-end
-
-# Get all options in a group
-function group_options(verbosity::DDEVerbosity, group::Symbol)
-    if group === :delay_specific
-        return NamedTuple{delay_specific_options}(getproperty(verbosity, opt)
-                                                  for opt in delay_specific_options)
-    else
-        error("Unknown group: $group")
-    end
-end
-
-function DDEVerbosity(;
-        ode_verbosity = nothing, delay_specific = nothing, kwargs...)
-    # Validate group arguments
-    if delay_specific !== nothing && !(delay_specific isa AbstractMessageLevel)
-        throw(ArgumentError("delay_specific must be a SciMLLogging.AbstractMessageLevel, got $(typeof(delay_specific))"))
-    end
-
-    if ode_verbosity !== nothing && !(ode_verbosity isa ODEVerbosity)
-        throw(ArgumentError("ode_verbosity must be an ODEVerbosity, got $(typeof(ode_verbosity))"))
-    end
-
-    # Validate individual kwargs
-    for (key, value) in kwargs
-        if !(key in delay_specific_options)
-            throw(ArgumentError("Unknown verbosity option: $key. Valid options are: $(delay_specific_options)"))
-        end
-        if !(value isa AbstractMessageLevel)
-            throw(ArgumentError("$key must be a SciMLLogging.AbstractMessageLevel, got $(typeof(value))"))
-        end
-    end
-
-    # Build arguments using NamedTuple for type stability
-    default_args = (
-        ode_verbosity = ode_verbosity === nothing ? ODEVerbosity() : ode_verbosity,
-        discontinuity_tracking = Silent(),
-        delay_evaluation = Silent(),
-        constrained_step = Silent(),
-        residual_control = Silent(),
-        neutral_delay = Silent(),
-        state_dependent_delay = Silent()
+@verbosity_specifier DDEVerbosity begin
+    toggles = (
+        :ode_verbosity,
+        :discontinuity_tracking,
+        :delay_evaluation,
+        :constrained_step,
+        :residual_control,
+        :neutral_delay,
+        :state_dependent_delay
     )
 
-    # Apply group-level settings
-    final_args = if delay_specific !== nothing
-        NamedTuple{keys(default_args)}(
-            _resolve_arg_value(key, default_args[key], delay_specific)
-            for key in keys(default_args)
-        )
-    else
-        default_args
-    end
-
-    # Apply individual overrides
-    if !isempty(kwargs)
-        final_args = merge(final_args, NamedTuple(kwargs))
-    end
-
-    DDEVerbosity(values(final_args)...)
-end
-
-# Constructor for verbosity presets following the hierarchical levels:
-# None < Minimal < Standard < Detailed < All
-# Each level includes all messages from levels below it plus additional ones
-function DDEVerbosity(verbose::AbstractVerbosityPreset)
-    if verbose isa Minimal
-        # Minimal: Only fatal errors and critical warnings
-        DDEVerbosity(
+    presets = (
+        None = (
+            ode_verbosity = ODEVerbosity(None()),
+            discontinuity_tracking = Silent(),
+            delay_evaluation = Silent(),
+            constrained_step = Silent(),
+            residual_control = Silent(),
+            neutral_delay = Silent(),
+            state_dependent_delay = Silent()
+        ),
+        Minimal = (
             ode_verbosity = ODEVerbosity(Minimal()),
             discontinuity_tracking = Silent(),
             delay_evaluation = Silent(),
@@ -155,13 +86,17 @@ function DDEVerbosity(verbose::AbstractVerbosityPreset)
             residual_control = Silent(),
             neutral_delay = Silent(),
             state_dependent_delay = WarnLevel()
-        )
-    elseif verbose isa Standard
-        # Standard: Everything from Minimal + non-fatal warnings
-        DDEVerbosity()
-    elseif verbose isa Detailed
-        # Detailed: Everything from Standard + debugging/solver behavior
-        DDEVerbosity(
+        ),
+        Standard = (
+            ode_verbosity = ODEVerbosity(),
+            discontinuity_tracking = Silent(),
+            delay_evaluation = Silent(),
+            constrained_step = Silent(),
+            residual_control = Silent(),
+            neutral_delay = Silent(),
+            state_dependent_delay = Silent()
+        ),
+        Detailed = (
             ode_verbosity = ODEVerbosity(Detailed()),
             discontinuity_tracking = InfoLevel(),
             delay_evaluation = InfoLevel(),
@@ -169,10 +104,8 @@ function DDEVerbosity(verbose::AbstractVerbosityPreset)
             residual_control = InfoLevel(),
             neutral_delay = InfoLevel(),
             state_dependent_delay = WarnLevel()
-        )
-    elseif verbose isa All
-        # All: Maximum verbosity - every possible logging message at InfoLevel
-        DDEVerbosity(
+        ),
+        All = (
             ode_verbosity = ODEVerbosity(All()),
             discontinuity_tracking = InfoLevel(),
             delay_evaluation = InfoLevel(),
@@ -181,38 +114,33 @@ function DDEVerbosity(verbose::AbstractVerbosityPreset)
             neutral_delay = InfoLevel(),
             state_dependent_delay = InfoLevel()
         )
-    end
-end
+    )
 
-@inline function DDEVerbosity(verbose::None)
-    DDEVerbosity(
-        ODEVerbosity(None()),
-        Silent(),
-        Silent(),
-        Silent(),
-        Silent(),
-        Silent(),
-        Silent()
+    groups = (
+        delay_specific = (
+            :discontinuity_tracking,
+            :delay_evaluation,
+            :constrained_step,
+            :residual_control,
+            :neutral_delay,
+            :state_dependent_delay
+        ),
     )
 end
 
-# Helper function to resolve argument values based on group membership
-@inline function _resolve_arg_value(key::Symbol, default_val, delay_specific)
-    if key === :ode_verbosity
-        return default_val
-    elseif key in delay_specific_options && delay_specific !== nothing
-        return delay_specific
-    else
-        return default_val
-    end
-end
-
 function Base.getproperty(v::DDEVerbosity, s::Symbol)
-    if s in fieldnames(DDEVerbosity)
+    # For parametric types, we need to use the base type
+    DDE_fields = fieldnames(typeof(v).name.wrapper)
+    if s in DDE_fields
         return getfield(v, s)
-    elseif s in fieldnames(ODEVerbosity)
-        return getfield(getfield(v, :ode_verbosity), s)
     else
-        return error("type DDEVerbosity has no field ", s)
+        # Try to delegate to ODE verbosity
+        ode_v = getfield(v, :ode_verbosity)
+        ODE_fields = fieldnames(typeof(ode_v).name.wrapper)
+        if s in ODE_fields
+            return getfield(ode_v, s)
+        else
+            return error("type DDEVerbosity has no field ", s)
+        end
     end
 end
